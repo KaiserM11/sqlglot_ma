567cfa1fad5a1a00dc4d6eda32c8ffd300994476
KaiserM11 <matekaiser@mountanalytics.nl>
2024-02-23 11:52:49 +0100

    concat trial 3

--- sqlglot/generator.py
+++ sqlglot/generator.py
@@ -2400,12 +2400,12 @@ class Generator(metaclass=_Generator):
 
     def concat_sql(self, expression: exp.Concat) -> str:
         expressions = self.convert_concat_args(expression)
-
+        variables = [x.this for x in expressions]
         # Some dialects don't allow a single-argument CONCAT call
         if not self.SUPPORTS_SINGLE_ARG_CONCAT and len(expressions) == 1:
             return self.sql(expressions[0])
 
-        return f"TEXT_COMBINE({expressions})"
+        return f"TEXT_COMBINE({variables})"
 
     def concatws_sql(self, expression: exp.ConcatWs) -> str:
         return self.func(
