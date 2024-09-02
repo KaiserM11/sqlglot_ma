193f7c4b62ebb9271df6e13cdcd97929185a80eb
KaiserM11 <matekaiser@mountanalytics.nl>
2024-09-02 13:58:41 +0200

    Modified __init__ for ma dialect

--- sqlglot/dialects/__init__.py
+++ sqlglot/dialects/__init__.py
@@ -84,3 +84,4 @@ from sqlglot.dialects.teradata import Teradata
 from sqlglot.dialects.trino import Trino
 from sqlglot.dialects.tsql import TSQL
 from sqlglot.dialects.hana import HANA
+from sqlglot.dialects.ma import MA
